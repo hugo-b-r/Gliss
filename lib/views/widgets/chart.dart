e058c08eff7a7caf5fc2de8a70c0c02ef437ccab
Hugo Berthet-Rambaud <hugoberthetrambaud@outlook.com>
2025-05-11 22:09:46 +0200

    remove unused import in chart

--- lib/views/widgets/chart.dart
+++ lib/views/widgets/chart.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
-import 'package:gliding_aid/views/view_models/flight_view_model.dart';
 import 'package:gliding_aid/views/view_models/map_view_model.dart';
 import 'package:provider/provider.dart';
 
