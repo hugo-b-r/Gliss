ebce434aeff53084e2bab2df928916dedede508d
Hugo Berthet-Rambaud <hugoberthetrambaud@outlook.com>
2025-07-25 15:35:33 +0200

    add settings page

--- lib/Pages/settings_page.dart
+++ lib/Pages/settings_page.dart
@@ -16,7 +16,7 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Settings'),
+        title: null,
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
       body: ListView(
